25093d950d582976f8bf2ea39f3c606ea9c6bdaf
Raphael Fontoura <raphael.fontoura@gmail.com>
2022-07-19 11:52:31 -0300

    Create Orders list

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 
 import Loading from './src/components/Loading';
-import Signin from './src/screens/Signin';
+import Home from './src/screens/Home';
 import { THEME } from './src/styles/theme';
 
 export default function App() {
@@ -17,7 +17,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-        {fontsLoaded ? <Signin /> : <Loading />}
+        {fontsLoaded ? <Home /> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/Home.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import { Heading, HStack, IconButton, Text, useTheme, VStack, FlatList, Pressable, Center } from 'native-base';
+import { SignOut, ChatTeardropText } from 'phosphor-react-native';
+import Logo from '../assets/logo_secondary.svg';
+import Filter from '../components/Filter';
+import Order, { OrderProps } from '../components/Order';
+import Button from '../components/Button';
+
+function Home() {
+
+  const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open');
+  const [orders, setOrders] = useState<OrderProps[]>([
+    { 
+      id: "123", 
+      patrimony: "12345",
+      when: "18/07/2022 as 10:00",
+      status: "open"
+    },
+    { 
+      id: "124", 
+      patrimony: "12346",
+      when: "18/07/2022 as 10:30",
+      status: "closed"
+    },
+    { 
+      id: "125", 
+      patrimony: "12345",
+      when: "18/07/2022 as 10:00",
+      status: "open"
+    },
+    { 
+      id: "126", 
+      patrimony: "12346",
+      when: "18/07/2022 as 10:30",
+      status: "closed"
+    },
+    { 
+      id: "127", 
+      patrimony: "12345",
+      when: "18/07/2022 as 10:00",
+      status: "open"
+    }
+  ])
+  const { colors } = useTheme();
+
+  return (
+    <VStack flex={1} pb={6} bg="gray.700">
+      <HStack
+        w="full"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="gray.600"
+        pt={12}
+        pb={5}
+        px={6}
+      >
+        <Logo />
+        <IconButton
+          icon={<SignOut size={26} color={colors.gray[300]} />}
+        />
+      </HStack>
+
+      <VStack flex={1} px={6}>
+        <HStack w="full" mt={8} mb={4} justifyContent="space-between" alignItems="center">
+          <Heading color="gray.100">
+            Meus chamados
+          </Heading>
+          <Text color="gray.200">
+            3
+          </Text>
+        </HStack>
+
+        <HStack space={3} mb={8}>
+          <Filter
+            type='open'
+            title='em andamento'
+            onPress={() => setStatusSelected('open')}
+            isActive={statusSelected === 'open'}
+          />
+          <Filter 
+            type='closed' 
+            title='fechados' 
+            onPress={() => setStatusSelected('closed')}
+            isActive={statusSelected === 'closed'}
+          />
+
+        </HStack>
+
+        <FlatList 
+          data={orders.filter(item => item.status === statusSelected)}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <Order data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 100 }}
+          ListEmptyComponent={() => (
+            <Center>
+              <ChatTeardropText color={colors.gray[300]} size={40} />
+              <Text color={"gray.300"} fontSize="xl" mt={6} textAlign="center">
+                Você ainda não possui {'\n'}
+                solicitações {statusSelected === "open" ? "em andamento" : "finalizados"}
+              </Text>
+            </Center>
+          )}
+        />
+
+        <Button title='Nova solicitação' />
+      </VStack>
+
+    </VStack>
+  )
+}
+
+export default Home
